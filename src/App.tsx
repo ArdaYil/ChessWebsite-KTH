a8f7a66041af3d5912be72ba54371595dacd1c40
Arda Yildirim <ardayildirim05@icloud.com>
2024-09-06 23:22:12 +0200

    Implement markup code for board

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 const App = () => {
-  return <h1>Hello World!</h1>;
+  const generateChessBoard = () => {
+    const board = [];
+
+    for (let file = 0; file < 8; file++) {
+      for (let rank = 0; rank < 8; rank++) {
+        board.push(
+          <div className={(file + rank) % 2 === 0 ? "light" : "dark"} />
+        );
+      }
+    }
+
+    return <div className="board">{board}</div>;
+  };
+
+  return generateChessBoard();
 };
 
 export default App;
