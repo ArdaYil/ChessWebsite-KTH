9c2278b851f84ea1d493cf3889b6ccab1b890a49
Arda Yildirim <ardayildirim05@icloud.com>
2024-09-07 12:57:53 +0200

    Implement board layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ const App = () => {
 
     for (let file = 0; file < 8; file++) {
       for (let rank = 0; rank < 8; rank++) {
+        const color = (file + rank) % 2 === 0 ? "light" : "dark";
         board.push(
-          <div className={(file + rank) % 2 === 0 ? "light" : "dark"} />
+          <div className={`square ${color}`}>
+            <img src="src/assets/pieces/white/king.png" />
+          </div>
         );
       }
     }
